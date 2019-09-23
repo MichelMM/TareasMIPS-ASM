dc6bff1e543d15cae4ba6b060d4f718a81036a4f
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2019-09-22 22:03:13 -0500

    Rename SumaYComparacion to SumaYComparacion.asm

--- Tarea 4/SumaYComparacion.asm
+++ Tarea 4/SumaYComparacion.asm
@@ -6,9 +6,9 @@ dowhile:
 	jal suma #saltamos a suma
 	div $s1,$s0,4 #variable = counter / 4;
 	slti $t1,$s1,20 #variable < 20
-	beqz $t1,exit #Terminar ciclo while si la condición no es cumplida
+	beqz $t1,exit #Terminar ciclo while si la condiciÃ³n no es cumplida
 	j dowhile #Reiniciar el ciclo
 suma:
 	add $s0,$s0,$s0 #counter = suma(counter, counter); 
-	jr $ra #salto al valor almacenado en ra, en este caso la línea 7
+	jr $ra #salto al valor almacenado en ra, en este caso la lÃ­nea 7
 exit:
